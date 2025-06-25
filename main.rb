918ec4e1229bd202962672ad560690abe620af51
Matt Thompson <mattthompson56@gmail.com>
2025-06-24 23:05:03 -0400

    Color working for prompts. Cleaned up code a bit. Now to start fixing the broken update board system

--- main.rb
+++ main.rb
@@ -2,30 +2,30 @@ require 'colorize'
 require_relative 'lib/player'
 require_relative 'lib/board'
 
-players = [Player.new('X'.colorize(:color => :light_grey), "player_1"), 
+players = [Player.new('X'.colorize(:color => :yellow), "player_1"), 
            Player.new('O'.colorize(:color => :light_yellow), "player_2")]
 
 board = Board.new(players)
 
-player_colors = {:player_1 => {:color => :light_grey},
-                 :player_2 => {:color => :light_yellow}}
-
-puts "\n\n"
-
-puts "Welcome to Tic Tac Toe!\n\n".colorize(:color => :light_red)
-
-game = "true"
-player_1_turn = "Player_1 is [X] colored ORANGE".colorize(:color => :yellow)
-player_2_turn = "Player_2 is [O] colored YELLOW\n\n".
-  colorize(:color => :light_yellow)
-
+# Create an array of each players prompt in the color of their symbol color.
+player_1_turn = "Player_1's Turn: ".colorize(:color => :yellow)
+player_2_turn = "Player_2's Turn: ".colorize(:color => :light_yellow)
 messages = [player_1_turn, player_2_turn]
+
+# True until the game has a winner, or a cat's game.
+game = "true"
+
+
+puts "\n"
+puts "Welcome to Tic Tac Toe!\n\n".colorize(:color => :light_red)
+puts = "Player_1 X] colored ORANGE".colorize(:color => :yellow)
+puts = "Player_2 is [O] colored YELLOW\n\n".colorize(:color => :light_yellow)
+
 
 puts "Pick the space you want by typing the".colorize(:color => :light_red)
 puts "(Number) and hitting the [ENTER] Key\n".colorize(:color => :light_red)
 
 while game == "true"
-
   players.each_with_index do |choice, index|
     board.show_board
     puts messages[index]
